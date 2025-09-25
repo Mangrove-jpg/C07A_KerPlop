02fda8cceff69ee74fbb6017354587a643933e43
Stephen Carmon <stephen_carmon@mines.edu>
2025-09-25 11:24:29 -0600

    Added Implementation to LevelSetup.Java

--- src/gameEngine/LevelSetup.java
+++ src/gameEngine/LevelSetup.java
@@ -13,32 +13,36 @@ public class LevelSetup {
 	
 	//Constructor for LevelSetup
 	public LevelSetup() {
-		
 	}
 	
 	//Set Level Number
 	public void createLevel(int levelNum){
-		
+		if (levelNum == 0) {
+			level1();
+		}
+		else if (levelNum == 1) {
+			level2();
+		}
 	}
 	
 	//Return array list of moving pieces
 	public ArrayList<Moveable> getMovingPieces(){
-		
+		return movingPieces;
 	}
 	
 	//Return array list of interactable pieces
-	public ArrayList<Moveable> getInteractingPieces{
-		
+	public ArrayList<Moveable> getInteractingPieces(){
+		return interactingPieces;
 	}
 	
 	//Returns array of Drawables on board
 	public Drawable[] getBoard(){
-		
+		return gameboard;
 	}
 	
 	//Set up data structures for level 1
 	public void level1() {
-				
+			
 	}
 			
 	//Set up data structures for level 2
