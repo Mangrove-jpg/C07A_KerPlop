86fe702be5d9dcc4ddd2bfebc84d0c2bd053c4a2
Stephen Carmon <stephen_carmon@mines.edu>
2025-09-25 11:02:37 -0600

    Added Level Setup Stub

---
+++ src/gameEngine/LevelSetup.java
@@ -0,0 +1,50 @@
+package gameEngine;
+
+import java.util.ArrayList;
+
+public class LevelSetup {
+	
+	private int levelNumber;
+	private int playerStartLocation;
+	
+	private Drawable[] gameboard;
+	private ArrayList<Moveable> movingPieces;
+	private ArrayList<Moveable> interactingPieces;
+	
+	//Constructor for LevelSetup
+	public LevelSetup() {
+		
+	}
+	
+	//Set Level Number
+	public void createLevel(int levelNum){
+		
+	}
+	
+	//Return array list of moving pieces
+	public ArrayList<Moveable> getMovingPieces(){
+		
+	}
+	
+	//Return array list of interactable pieces
+	public ArrayList<Moveable> getInteractingPieces{
+		
+	}
+	
+	//Returns array of Drawables on board
+	public Drawable[] getBoard(){
+		
+	}
+	
+	//Set up data structures for level 1
+	public void level1() {
+				
+	}
+			
+	//Set up data structures for level 2
+	public void level2() {
+				
+	}
+	
+	
+}
