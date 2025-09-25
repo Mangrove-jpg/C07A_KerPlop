17d8a95df82b47ff9b63629c31e4f470aadcc559
Stephen Carmon <stephen_carmon@mines.edu>
2025-09-25 15:34:30 -0600

    Fixed Gameboard size and added Lava class

---
+++ src/levelPieces/Lava.java
@@ -0,0 +1,22 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Lava extends GamePiece {
+	
+	public Lava(char symbol, String label, int location) {
+		super(symbol, label, location);
+	}
+
+	public InteractionResult interact(Drawable[] gameboard, int playerLocation) {
+		if (getLocation() == playerLocation - 1) {
+			return InteractionResult.KILL;
+		}
+		else {
+			return InteractionResult.NONE;
+		}
+		
+	}
+
+}
