fcf2deeef3b5eccff66a0dbba3fa0403e76b79b1
Joyce <jtzhou@mines.edu>
2025-09-28 12:27:43 -0600

    Update TestInteractions.java
    Wrote testTree

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -2,13 +2,57 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestInteractions {
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import levelPieces.*;
 
+
+// Tests for all interactive game pieces
+
+class TestInteractions {
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	// Test that Tree never interacts
+	void testTree() {
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Tree tree = new Tree('T', "Tree", 12);
+		gameBoard[12] = tree;
+		
+		assertEquals(InteractionResult.NONE, tree.interact(gameBoard, 12));
+		
+		// Tree should never interact regardless of position
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++)
+			assertEquals(InteractionResult.NONE, tree.interact(gameBoard, i));
+	}
+	
+	@Test
+	void testPrize() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	void testLava() {
+		fail("Not implemented");
+		
+	}
+	
+	@Test
+	void testPortal() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	void testDragon() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	void testArcher() {
+		fail("Not implemented");
 	}
 
 }
