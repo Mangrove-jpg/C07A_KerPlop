04c4af7c3c6fa338e4f1a1007c17313874aeca44
Stephen Carmon <stephen_carmon@mines.edu>
2025-09-28 12:46:03 -0600

    Added test cases for all LevelPieces that interact with player when overlapping with it

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -15,7 +15,7 @@ import levelPieces.*;
 
 class TestInteractions {
 	
-	@Test
+	//@Test
 	// Test that Tree never interacts
 	void testTree() {
 		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
@@ -29,28 +29,79 @@ class TestInteractions {
 			assertEquals(InteractionResult.NONE, tree.interact(gameBoard, i));
 	}
 	
-	@Test
+	//@Test
 	void testPrize() {
-		fail("Not implemented");
+		// Initialize Prize and Gameboard
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Prize myPrize = new Prize('$', "Prize", 12);
+		gameBoard[12] = myPrize;
+		
+		//Test Interact results in GET_POINT when Player and Prize overlap
+		assertEquals(InteractionResult.GET_POINT, myPrize.interact(gameBoard, 12));
+		
+		//Test Prize Disappears after collecting
+		assertNull(gameBoard[12]);
+		
+		// Prize should never interact if at different position
+		for (int i = 0; i < 12; i++)
+			assertEquals(InteractionResult.NONE, myPrize.interact(gameBoard, i));
+		for (int i = 13; i < GameEngine.BOARD_SIZE; i++)
+			assertEquals(InteractionResult.NONE, myPrize.interact(gameBoard, i));
 	}
 	
-	@Test
+	//@Test
 	void testLava() {
-		fail("Not implemented");
+		// Initialize Lava and Gameboard
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Lava myLava = new Lava('L', "Lava", 12);
+		gameBoard[12] = myLava;
+				
+		//Test Interact results in Kill when Player and Lava overlap
+		assertEquals(InteractionResult.KILL, myLava.interact(gameBoard, 12));
+				
+		// Lava should never interact when player and lava don't overlap
+		for (int i = 0; i < 12; i++)
+			assertEquals(InteractionResult.NONE, myLava.interact(gameBoard, i));
+		for (int i = 13; i < GameEngine.BOARD_SIZE; i++)
+			assertEquals(InteractionResult.NONE, myLava.interact(gameBoard, i));
 		
 	}
 	
-	@Test
+	//@Test
 	void testPortal() {
-		fail("Not implemented");
+		// Initialize Portal and Gameboard
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Portal myPortal = new Portal('O', "Portal", 12);
+		gameBoard[12] = myPortal;
+						
+		//Test Interact results in ADVANCE when Player and Portal overlap
+		assertEquals(InteractionResult.ADVANCE, myPortal.interact(gameBoard, 12));
+						
+		// Portal should never interact when player and Portal don't overlap
+		for (int i = 0; i < 12; i++)
+			assertEquals(InteractionResult.NONE, myPortal.interact(gameBoard, i));
+		for (int i = 13; i < GameEngine.BOARD_SIZE; i++)
+			assertEquals(InteractionResult.NONE, myPortal.interact(gameBoard, i));
 	}
 	
-	@Test
+	//@Test
 	void testDragon() {
-		fail("Not implemented");
+		// Initialize Portal and Gameboard
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Dragon myDragon = new Dragon('D', "Dragon", 12);
+		gameBoard[12] = myDragon;
+								
+		//Test Interact results in HIT when Player and Dragon overlap
+		assertEquals(InteractionResult.HIT, myDragon.interact(gameBoard, 12));
+								
+		// Dargon should never interact when player and Dragon don't overlap
+		for (int i = 0; i < 12; i++)
+			assertEquals(InteractionResult.NONE, myDragon.interact(gameBoard, i));
+		for (int i = 13; i < GameEngine.BOARD_SIZE; i++)
+			assertEquals(InteractionResult.NONE, myDragon.interact(gameBoard, i));
 	}
 	
-	@Test
+	//@Test
 	void testArcher() {
 		fail("Not implemented");
 	}
